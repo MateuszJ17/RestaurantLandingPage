0da561d05eb88eeb5633ce254fed3cee737f0985
Mateusz <m.jaroszewski17@gmail.com>
2020-02-17 20:28:49 +0100

    About section (not mobile friendly)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { AboutDescriptionComponent } from './components/about-description/about-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    AboutDescriptionComponent
   ],
   imports: [
     BrowserModule,
